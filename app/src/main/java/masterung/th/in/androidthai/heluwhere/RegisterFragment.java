550839882390f789ae06e0c592c6470ad7d787ab
masterUNG <phrombutr@gmail.com>
2019-02-13 14:28:08 +0700

    update

--- app/src/main/java/masterung/th/in/androidthai/heluwhere/RegisterFragment.java
+++ app/src/main/java/masterung/th/in/androidthai/heluwhere/RegisterFragment.java
@@ -11,19 +11,19 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment {
+public class RegisterFragment extends Fragment {
 
 
-    public MainFragment() {
+    public RegisterFragment() {
         // Required empty public constructor
-    }   // Constructor
+    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
-    }   // Create View
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
 
-}   // Main Class
+}
